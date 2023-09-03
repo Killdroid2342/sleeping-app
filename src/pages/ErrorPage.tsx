7c673127f6843c6a91379aaac40a9b2b54cd7473
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-04 00:25:26 +0100

    finished error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,19 @@
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>this is an error page</h1>
+    <div className='flex h-screen bg-neutral-200 items-center justify-center'>
+      <div className='flex flex-col'>
+        <h1 className='text-center text-gray-800 text-9xl'>404 error</h1>
+        <p className='text-center p-10 text-2xl'>This page doesn't exist</p>
+        <p className='text-center text-2xl'>
+          Would you like to{' '}
+          <a
+            className='text-red-900 cursor-pointer'
+            href='https://www.codecademy.com/article/http-errors-404'
+          >
+            learn about HTTP errors?
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
