88ecfea92b48f04a508b303f08e92635c0976465
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-05 12:51:08 +0100

    created nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,73 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const navClick = () => {
+    setOpen(!open);
+  };
+
+  const [isTransformed, setTransform] = useState(false);
+
+  const toggleTransform = () => {
+    setTransform(!isTransformed);
+  };
+
+  return (
+    <div
+      className={`fixed top-0 left-0 h-full w-0 bg-stone-900 text-white transition-all transform ease-in-out duration-300 ${
+        open ? 'w-64' : ''
+      }`}
+    >
+      <div onClick={navClick} className='mt-10 ml-10'>
+        <div className='cursor-pointer' onClick={toggleTransform}>
+          <div
+            className={`w-8 h-1 bg-white mb-1 transition-transform transform ${
+              isTransformed ? 'rotate-45 translate-y-2' : ''
+            }`}
+          ></div>
+          <div className={`w-8 h-1 bg-white mb-1 opacity-100`}></div>
+          <div
+            className={`w-8 h-1 bg-white ${
+              isTransformed ? '-rotate-45 -translate-y-2' : ''
+            }`}
+          ></div>
+        </div>
+      </div>
+      {open && (
+        <ul>
+          <li className='mt-20 text-white font-bold text-xl text-center'>
+            {'Account goes here'}
+          </li>
+          <li
+            className='mt-5 text-white text-center cursor-pointer'
+            onClick={() => navigate('/')}
+          >
+            Sleep
+          </li>
+          <li
+            className='mt-5 text-white text-center cursor-pointer'
+            onClick={() => navigate('/Schedule')}
+          >
+            Schedule
+          </li>
+          <li
+            className='mt-5 text-white text-center cursor-pointer'
+            onClick={() => navigate('/Progress')}
+          >
+            Progress
+          </li>
+          <li
+            className='mt-5 text-white text-center cursor-pointer'
+            onClick={() => navigate('/LoginPage')}
+          >
+            Login/Sign-Up
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Nav;
