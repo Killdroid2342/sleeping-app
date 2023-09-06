dae413dc9da9abe0befe5b48cc4f284e2c396bf3
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-07 00:12:20 +0100

    created creating user function and hash passwords

--- server/modal/user.js
+++ server/modal/user.js
@@ -9,3 +9,35 @@ const conn = mysql.createConnection({
   password: '',
   database: 'sleep-tracker',
 });
+const createUser = async (username, password) => {
+  conn.query('INSERT INTO users (username, password) VALUES (?,?)', [
+    username,
+    password,
+  ]);
+};
+const hashPassword = async (password, saltRounds) => {
+  const res = bcrypt.hashSync(password, saltRounds);
+  return res;
+};
+const isUserExists = async (username) => {
+  const res = conn
+    .promise()
+    .query('SELECT * FROM users WHERE username = ?', [username])
+    .then(([rows, fields]) => {
+      if (rows.length > 0) {
+        return rows[0];
+      } else {
+        return false;
+      }
+    });
+  return res;
+};
+async function comparePassswords(passwords, hash) {
+  return bcrypt.compareSync(passwords, hash);
+}
+module.exports = {
+  createUser,
+  hashPassword,
+  isUserExists,
+  comparePassswords,
+};
