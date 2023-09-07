2b20cb2a3e1491dfe9d567a20e358b3d3538606e
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-08 00:12:38 +0100

    changing app ideas

--- src/pages/Sleep.tsx
+++ src/pages/Sleep.tsx
@@ -3,13 +3,12 @@ import '../index.css';
 
 const Main = () => {
   return (
-    <div className='flex h-screen background'>
+    <div className='background h-screen'>
       <Nav />
-      <div className='flex-1 p-4'>
+      <div className='p-4 flex flex-col items-center justify-center flex-1'>
         <h1 className='text-center text-2xl font-bold text-white'>
           Sleep Tracker
         </h1>
-        {/* this page is the  sleep duration and the sleep stats*/}
       </div>
     </div>
   );
