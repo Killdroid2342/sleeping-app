bd7c726e08d58dda7adac5d90e605b299790a647
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-08 00:12:28 +0100

    created token

---
+++ server/modal/token.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+async function verifyToken(token) {
+  try {
+    const res = jwt.verify(token, process.env.TokenA);
+    return res;
+  } catch (err) {
+    return false;
+  }
+}
+
+const jwtToken = (username) => {
+  const jwtSignin = jwt.sign(
+    {
+      username,
+    },
+    process.env.TokenA,
+    { expiresIn: '7d' }
+  );
+  return jwtSignin;
+};
+
+module.exports = {
+  verifyToken,
+  jwtToken,
+};
