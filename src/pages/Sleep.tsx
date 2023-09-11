f18c5f44a6b281192e3688a3fdc12247331580ed
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-12 00:15:16 +0100

    implemented modal on sleep

--- src/pages/Sleep.tsx
+++ src/pages/Sleep.tsx
@@ -1,14 +1,58 @@
+import { decodeToken } from 'react-jwt';
 import Nav from '../components/Nav';
 import '../index.css';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+import Modal from '../components/Modal';
 
 const Main = () => {
+  const [clientUsername, setClientUsername] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const username = () => {
+    const getJWT = Cookies.get('UserjwtToken');
+    if (getJWT) {
+      const decodedTokenUsername = (decodeToken(getJWT) as { username: string })
+        .username;
+      setClientUsername(decodedTokenUsername);
+      setIsLoggedIn(true);
+    } else {
+      setClientUsername('Not Logged In. Log in to use all features');
+      setIsLoggedIn(false);
+    }
+  };
+
+  useEffect(() => {
+    username();
+  });
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const close = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className='background h-screen'>
-      <Nav />
+      <Nav clientUsername={clientUsername} />
       <div className='p-4 flex flex-col items-center justify-center flex-1'>
-        <h1 className='text-center text-2xl font-bold text-white'>
+        <h1 className='text-center text-3xl font-bold text-white'>
           Sleep Tracker
         </h1>
+        <input
+          type='button'
+          value='New Entry +'
+          className='text-white border border-white p-3 rounded-2xl font-bold text-xl mt-10'
+          onClick={openModal}
+        />
+        {isModalOpen && <Modal close={close} isLoggedIn={isLoggedIn} />}
+        <div className='flex flex-row justify-center border border-white mt-80'>
+          <div className='border border-red-950 text-white'>Sleep Duration</div>
+          <div className='border border-blue-950 text-white'>Sleep Stats</div>
+        </div>
       </div>
     </div>
   );
