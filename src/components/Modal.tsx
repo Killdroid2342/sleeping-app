a74a86de84fdc28db6b926608d8d2225db2a9a26
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-14 00:10:43 +0100

    added new input and moved state and type

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
 
-type Result = {
-  sleepingHours: string;
-  timeOfSleep: string;
-  wakeUpTime: string;
-};
-
-const Modal = ({ close, isLoggedIn }: any) => {
+const Modal = ({ close, isLoggedIn, prevData, setPrevData }: any) => {
   const [data, setData] = useState({
     sleepingHours: '',
     timeOfSleep: '',
     wakeUpTime: '',
+    day: '',
   });
-
-  const [prevData, setPrevData] = useState<Result[]>([]);
 
   async function submitForm(e: any) {
     e.preventDefault();
     const newResult = { ...data };
-    setPrevData((setPrevData) => [...setPrevData, newResult]);
+    setPrevData((setPrevData: any) => [...setPrevData, newResult]);
     setData({
       sleepingHours: '',
       timeOfSleep: '',
       wakeUpTime: '',
+      day: '',
     });
   }
 
@@ -34,11 +28,9 @@ const Modal = ({ close, isLoggedIn }: any) => {
     }));
   }
 
-  console.log(prevData);
-
   return (
-    <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
-      <div className='bg-white p-4 rounded-lg overflow-auto max-h-96'>
+    <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black z-50'>
+      <div className='bg-white p-4 rounded-lg overflow-hidden h-3/6'>
         <button
           className='mt-2 px-4 py-2 bg-gray-900 text-white rounded-lg'
           onClick={close}
@@ -47,7 +39,7 @@ const Modal = ({ close, isLoggedIn }: any) => {
         </button>
         <h2 className='text-2xl font-semibold mb-4'></h2>
         {isLoggedIn ? (
-          <form className='flex flex-col items-center' onSubmit={submitForm}>
+          <form className='flex flex-col items-center ' onSubmit={submitForm}>
             <input
               type='text'
               placeholder='Enter Sleeping Hours'
@@ -69,10 +61,19 @@ const Modal = ({ close, isLoggedIn }: any) => {
             <input
               type='text'
               placeholder='Enter Wake Up Time'
-              className='text-black border border-black p-3 rounded-2xl font-bold text-xl focus:outline-none'
+              className='text-black border border-black p-3 rounded-2xl font-bold text-xl focus:outline-none mb-3'
               onChange={loginInput}
               name='wakeUpTime'
               value={data.wakeUpTime}
+              required
+            />
+            <input
+              type='date'
+              placeholder='day'
+              className='text-black border border-black p-3 rounded-2xl font-bold text-xl focus:outline-none'
+              onChange={loginInput}
+              name='day'
+              value={data.day}
               required
             />
             <input
