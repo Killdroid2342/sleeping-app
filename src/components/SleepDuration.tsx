82fe5acb5be6655150d0ef3b7f14edba607fb86a
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-15 00:35:07 +0100

    implemented graph with data

--- src/components/SleepDuration.tsx
+++ src/components/SleepDuration.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
 import { LineChart, Line, YAxis, XAxis } from 'recharts';
 
-const SleepDuration = ({ prevData }: any) => {
+const SleepDuration = ({ prevData, isLoggedIn }: any) => {
   return (
-    <>
+    <div className='mr-10'>
       <p className='text-center text-2xl font-bold'>Sleep Duration</p>
-      <LineChart width={400} height={400} data={prevData}>
-        <Line type='monotone' dataKey='sleepingHours' stroke='#8884d8' />
-        <YAxis />
-        <XAxis dataKey='day' />
-      </LineChart>
-    </>
+      {isLoggedIn ? (
+        <LineChart width={400} height={400} data={prevData}>
+          <Line type='monotone' dataKey='hoursofsleep' stroke='#6203fc' />
+          <YAxis axisLine={{ stroke: 'white' }} tick={{ fill: 'white' }} />
+          <XAxis
+            axisLine={{ stroke: 'white' }}
+            tick={{ fill: 'white' }}
+            dataKey='date'
+          />
+        </LineChart>
+      ) : (
+        <p>log in to see data</p>
+      )}
+    </div>
   );
 };
 
