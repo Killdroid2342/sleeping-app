cb0e820600f2faaa21950d15af87070b545c59ab
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-16 00:17:45 +0100

    removed ternary operator

--- src/components/SleepStats.tsx
+++ src/components/SleepStats.tsx
@@ -1,45 +1,41 @@
-const SleepStats = ({ prevData, isLoggedIn }: any) => {
+const SleepStats = ({ prevData }: any) => {
   return (
     <div className='ml-10'>
       <p className='text-center text-2xl font-bold'>Sleep Stats</p>
-      {isLoggedIn ? (
-        <div className='flex flex-row'>
-          <ul>
-            <li className='text-center p-2 font-bold'>Date</li>
-            {prevData.map((data: any, index: number) => (
-              <li key={index} className='text-center'>
-                {data.date}
-              </li>
-            ))}
-          </ul>
-          <ul>
-            <li className='text-center p-2 font-bold'>Bed Time</li>
-            {prevData.map((data: any, index: number) => (
-              <li key={index} className='text-center'>
-                {data.bedtime}
-              </li>
-            ))}
-          </ul>
-          <ul>
-            <li className='text-center p-2 font-bold'>Wake Up Time</li>
-            {prevData.map((data: any, index: number) => (
-              <li key={index} className='text-center'>
-                {data.wakeUpTime}
-              </li>
-            ))}
-          </ul>
-          <ul>
-            <li className='text-center p-2 font-bold'>Hours Of Sleep</li>
-            {prevData.map((data: any, index: number) => (
-              <li key={index} className='text-center'>
-                {data.hoursofsleep}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>log in to see data</p>
-      )}
+      <div className='flex flex-row'>
+        <ul>
+          <li className='text-center p-2 font-bold'>Date</li>
+          {prevData.map((data: any, index: number) => (
+            <li key={index} className='text-center'>
+              {data.date}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          <li className='text-center p-2 font-bold'>Bed Time</li>
+          {prevData.map((data: any, index: number) => (
+            <li key={index} className='text-center'>
+              {data.bedtime}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          <li className='text-center p-2 font-bold'>Wake Up Time</li>
+          {prevData.map((data: any, index: number) => (
+            <li key={index} className='text-center'>
+              {data.wakeUpTime}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          <li className='text-center p-2 font-bold'>Hours Of Sleep</li>
+          {prevData.map((data: any, index: number) => (
+            <li key={index} className='text-center'>
+              {data.hoursofsleep}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
