f912b60c3edd26de25b0d6fd8eb36704c3d355be
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-16 00:17:33 +0100

    removed ternary operator

--- src/components/SleepDuration.tsx
+++ src/components/SleepDuration.tsx
@@ -1,22 +1,18 @@
 import { LineChart, Line, YAxis, XAxis } from 'recharts';
 
-const SleepDuration = ({ prevData, isLoggedIn }: any) => {
+const SleepDuration = ({ prevData }: any) => {
   return (
     <div className='mr-10'>
       <p className='text-center text-2xl font-bold'>Sleep Duration</p>
-      {isLoggedIn ? (
-        <LineChart width={400} height={400} data={prevData}>
-          <Line type='monotone' dataKey='hoursofsleep' stroke='#6203fc' />
-          <YAxis axisLine={{ stroke: 'white' }} tick={{ fill: 'white' }} />
-          <XAxis
-            axisLine={{ stroke: 'white' }}
-            tick={{ fill: 'white' }}
-            dataKey='date'
-          />
-        </LineChart>
-      ) : (
-        <p>log in to see data</p>
-      )}
+      <LineChart width={400} height={400} data={prevData}>
+        <Line type='monotone' dataKey='hoursofsleep' stroke='#6203fc' />
+        <YAxis axisLine={{ stroke: 'white' }} tick={{ fill: 'white' }} />
+        <XAxis
+          axisLine={{ stroke: 'white' }}
+          tick={{ fill: 'white' }}
+          dataKey='date'
+        />
+      </LineChart>
     </div>
   );
 };
