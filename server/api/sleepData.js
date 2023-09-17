33a157b4450cd73d9ce1a183f7381d28fa033ba5
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-18 00:06:42 +0100

    fixed post request

--- server/api/sleepData.js
+++ server/api/sleepData.js
@@ -5,12 +5,15 @@ require('dotenv').config();
 
 const { insertItems } = require('../modal/sleepData');
 
-router.get('/addToDatabase', async (req, res) => {
-  const data = [{}];
-  data.map((index) => {
-    const { date, bedtime, wakeUpTime, hoursofsleep } = index;
-    // insertItems(date, bedtime, wakeUpTime, hoursofsleep);
-  });
-  res.send('Hello');
+router.use(bodyParser.json());
+
+router.post('/addToDatabase', async (req, res) => {
+  const { date, bedtime, wakeUpTime, hoursofsleep } = req.body;
+  console.log(date, bedtime, wakeUpTime, hoursofsleep, 'DATA');
+  try {
+    insertItems(date, bedtime, wakeUpTime, hoursofsleep);
+  } catch (e) {
+    console.log(e);
+  }
 });
 module.exports = router;
